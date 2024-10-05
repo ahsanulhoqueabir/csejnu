31d3b77d1ac1cea77b532691eb00fc51d40466d4
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2024-10-05 10:37:13 +0600

    mmongodb string modified

--- api/controllers/authC.js
+++ api/controllers/authC.js
@@ -96,7 +96,8 @@ const birthdayMail = async (req, res) => {
       today,
     });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    // res.status(500).json({ message: err.message });
+    res.status(500).json(err);
   }
 };
 module.exports = {
